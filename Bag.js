778094d2e064e65e03c79e00e5e09c85fe13fa21
aamcodes <aaronmendezcodes@gmail.com>
2023-02-23 14:12:12 -0500

    Finished bag.

--- Bag.js
+++ Bag.js
@@ -1,8 +1,14 @@
 class Bag {
-    // Include constructor Method with the following property: 
-        // 1. Weight
-            //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+	// Include constructor Method with the following property:
+	// 1. Weight
+	//1a. Should handle an exception in case user adds no weight and throw an error.
+	constructor(weight) {
+		this.weight = weight;
+		if (!weight) {
+			throw new Error('bag needs weight');
+		}
+	}
 }
 
 //Export the module
+module.exports = Bag;
